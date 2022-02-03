a156aaf4f32a2c1ade2d7646261d3841b40e3cfd
sayra17 <agussayra03@gmail.com>
2022-02-03 19:26:44 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+04-Javascript-TUGAS-13-JS
---
+++ tugas13.js
@@ -0,0 +1,7 @@
+function buah_buahan() {
+    var buah = ['Pisang', 'Jeruk']
+    console.log(buah)
+    buah.unshift('Apel', 'Mangga')
+    return buah
+}
+console.log(buah_buahan());
